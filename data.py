18611710b73c15a19512edd6cc61d38b478b0f53
eEasonchen <easonc9@nycstudents.net>
2024-02-02 12:27:23 -0500

    Create data.py

---
+++ data.py
@@ -0,0 +1,35 @@
+""" def add(x,y):
+    print(x + y)
+add(1,2)
+
+name = "Eason"
+def greeting(person):
+    print(person)
+greeting (name)
+#booLeans
+tenure = False
+def is_tenured(status):
+    if(status == True):
+        print("They have tenure")
+    else:
+        print("They are not tenured")
+
+is_tenured(tenure)
+
+x = 3
+y = float(3)
+print(x,y) """
+
+values = [1,2.23,5,7,2,30,15]
+print(values)
+for i in values:
+    print(i)
+
+print(values[0])
+print(values[6])
+
+x = 
+y= x.split( )
+z = y[0]
+print(y)
+print(z)
