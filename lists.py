731232a2c4da1af28716153e5e6bd0f96e436329
eEasonchen <easonc9@nycstudents.net>
2024-02-06 12:27:53 -0500

    Update lists.py

--- lists.py
+++ lists.py
@@ -5,37 +5,47 @@ animals = ["Zebra", "Camel", "Ape"]
 """ for animal in animals:
     if (animal == "Camel"):
         print("Camel") """
-""" x = "Hello Eason, you're the best" """
+""" x = "Hello Eason" """
 """ print(x[0])
 y = x.upper()
 print (y)
  """
-""" words_list = x.split( )
-print(len(words_list)) """
+
 
 
 """ day_of_week = input("what day is it? ")
 if day_of_week == "Friday":
     print("correct")
 else:
-    print("incorrect") """
+    print("incorrect") 
+words_list = x.split( )
+print(len(words_list)) 
+"""
 
 
 
-num = input("num:")
-if num  == "0":
-    print('neutral')
-elif num +2 == ["2","4","6","8","10"]:
-    print("even")
+num = int(input("number: "))
+if (num % 2)  == 0:
+    print('even')
 else: 
     print('odd')
 
-""" tip = input("tip%:")
-if tip == "0%":
-    print("bad")
-if tip == "15%":
-    print("okay")
-if tip == "20%":
-    print("good")
-if tip == "25%":
-    print("great") """
+""" service = input("service was:")
+if service == "bad":
+    print("0% tip")
+elif service == "okay":
+    print("15% tip")
+elif service == "good":
+    print("20% tip")
+elif service == "great":
+    print("25% tip") """
+
+""" x = input("ailsdfhgbiolsdf")
+if(int(x) == ["2","4","6","8","10"]):
+    print("even") """
+
+""" number = int(input("two numbers: "))
+print("Factors: " .format(number))
+for i in range(1, number + 1):
+    if(number % i == 0):
+        print (i) """
